8bb6776d9bcaf83fef3809ba8c8a647a49f63366
Muhtarom Zain <muhtaromzain@gmail.com>
2021-08-05 21:40:08 +0800

    Retrieve data from database

--- controller.js
+++ controller.js
@@ -7,3 +7,28 @@ var connection = require('./connection');
 exports.index = function (req, res) {
 	response.ok("Apps Run", res);
 };
+
+// Get All Data from Database
+exports.getData = function (req, res) {
+	connection.query('SELECT * FROM users', function(error, rows, fields){
+		if(error) {
+			console.log(error);
+		}
+		else {
+			response.ok(rows, res);
+		}
+	});
+};
+
+// Get Data based on id from Database
+exports.getDataUser = function (req, res) {
+	let id = req.params.id;
+	connection.query('SELECT * FROM users WHERE ID = ?', [id],function(error, rows, fields){
+		if(error) {
+			console.log(error);
+		}
+		else {
+			response.ok(rows, res);
+		}
+	});
+};
--- router.js
+++ router.js
@@ -4,4 +4,8 @@ module.exports = function(app) {
 	var json = require('./controller');
 	
 	app.route('/').get(json.index);
+	
+	app.route('/get').get(json.getData);
+	
+	app.route('/get/:id').get(json.getDataUser);
 };

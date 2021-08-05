4ae965d65c4c0044a7fbf3a231233293b7e6227e
Muhtarom Zain <muhtaromzain@gmail.com>
2021-08-05 19:51:33 +0800

    Set routes in server.js

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+// call routes
+var routes = require('./router');
+routes(app);
+
 app.listen(3000);

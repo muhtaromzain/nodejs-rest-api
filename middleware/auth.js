35609852f4cc0b1cc3144d7217d1bf736f3e97cb
Muhtarom Zain <muhtaromzain@gmail.com>
2021-08-07 22:03:33 +0800

    Create login

--- middleware/auth.js
+++ middleware/auth.js
@@ -45,3 +45,61 @@ exports.registration = function(req, res) {
 		}
 	});
 };
+
+// controller for login
+exports.login = function(req, res) {
+	var post = {
+		email: req.body.email,
+		password: req.body.password
+	};
+	
+	var query = "SELECT * FROM ?? WHERE ??=? AND ??=?";
+	var table = ["users", "password", md5(post.password), "email", post.email];
+	
+	query = mysql.format(query, table);
+	connection.query(query, function(error, rows){
+		if(error) {
+			console.log(error);
+		}
+		else {
+			if(rows.length == 1) {
+				var token = jwt.sign({rows}, config.secret, {
+					expiresIn: 1440
+				});
+				
+				user_id = rows[0].id;
+				
+				var data = {
+					user_id: user_id,
+					token: token,
+					ip_address: ip.address()
+				};
+				
+				var query = "INSERT INTO ?? SET ?";
+				var table = ["access_token"];
+				
+				query = mysql.format(query, table);
+				
+				connection.query(query, data, function(error, rows) {
+					if(error) {
+						console.log(error);
+					}
+					else {
+						res.json({
+							success: true,
+							message: "Token generated",
+							token: token,
+							user: data.user_id
+						});
+					}
+				});
+			}
+			else {
+				res.json({
+					"error": true,
+					"message": "Email or Password are invalid"
+				});
+			}
+		}
+	}); 
+}
--- middleware/index.js
+++ middleware/index.js
@@ -3,5 +3,6 @@ var auth = require('./auth');
 var router = express.Router();
 
 router.post('/api/v1/register', auth.registration);
+router.post('/api/v1/login', auth.login);
 
 module.exports = router;

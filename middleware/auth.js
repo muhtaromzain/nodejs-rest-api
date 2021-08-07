ad191a00fcc59f8107b461356a28024a8297c15d
Muhtarom Zain <muhtaromzain@gmail.com>
2021-08-07 18:49:01 +0800

    Install md5. Create config and middleware auth

---
+++ middleware/auth.js
@@ -0,0 +1,47 @@
+var connection = require('../connection');
+var mysql = require('mysql');
+var md5 = require('MD5');
+var response = require('../res');
+var jwt = require('jsonwebtoken');
+var config = require('../config/secret');
+var ip = require('ip');
+
+// controller for registration
+exports.registration = function(req, res) {
+	var post = {
+		username: req.body.username,
+		email: req.body.email,
+		password: md5(req.body.password),
+		role_id: req.body.role_id,
+		created_at: new Date()
+	};
+	
+	var query = "SELECT email FROM ?? WHERE ??";
+	var table = ["users", "email", post.email];
+	
+	query = mysql.format(query.table);
+	
+	connection.query(query, function(error, rows){
+		if(error) {
+			console.log(error);
+		}
+		else {
+			if(rows.length == 0) {
+				var query = "INSERT INTO ?? SET ?";
+				var table = ["users"];
+				query = mysql.format(query, table);
+				connection.query(query, post, function(error, rows) {
+					if(error) {
+						console.log(error);
+					}
+					else {
+						response.ok("Registered succefully", res);
+					}
+				});
+			}
+			else {
+				response.ok("Email already registered!");
+			}
+		}
+	});
+};
